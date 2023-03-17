ef267c6622b0dfe2894704bfecc932e0bcf5866d
yoshuu <appcs342@gmail.com>
2023-03-17 17:40:35 +0800

    第三章 第一個題目

---
+++ book03/01.js
@@ -0,0 +1,12 @@
+var whatIsThis;
+var randomNumber = Math.random();
+if (randomNumber < 0.33) {
+    whatIsThis = 123;
+}
+else if (randomNumber < 0.66) {
+    whatIsThis = "string";
+}
+else {
+    whatIsThis = true;
+}
+whatIsThis;
---
+++ book03/01.ts
@@ -0,0 +1,16 @@
+// let whatIsThis;
+// let randomNumber = Math.random();
+
+// if (randomNumber < 0.33) {
+//   whatIsThis = 123;
+// } else if (randomNumber < 0.66) {
+//   whatIsThis = "string";
+// } else {
+//   whatIsThis = true;
+// }
+
+// whatIsThis;
+
+// let data: number | string | boolean;
+
+// data = [];
