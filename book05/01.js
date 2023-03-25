ca1d6891e8f89b507ea0f4cca89e0966bc8c2f9c
yoshuu <appcs342@gmail.com>
2023-03-25 21:30:26 +0800

    0325 02

---
+++ book05/01.js
@@ -0,0 +1,18 @@
+"use strict";
+// class Cat {
+//   name: string;
+//   breed: string;
+//   constructor(name: string, breed: string) {
+//     this.name = name;
+//     this.breed = breed;
+//   }
+// }
+// 這樣才完全不會報錯
+// class Aaa {
+//   // 私有的變數
+//   #name;
+//   constructor(name: string) {
+//     this.#name = name;
+//   }
+// }
+// const apple = new Aaa("aaa");
