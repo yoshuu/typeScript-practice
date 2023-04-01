359c4c697da01d4b2c3b77b107ce09b9ebcbeef4
yoshuu <appcs342@gmail.com>
2023-04-01 21:56:27 +0800

    book06 6-1

---
+++ book06/6-1.ts
@@ -0,0 +1,15 @@
+interface Vic {
+  name: string;
+
+  // 可加可不加，可選的意思
+  age?: number;
+
+  readonly sex: "man" | "female";
+}
+
+const vic: Vic = {
+  name: "vic",
+  sex: "man",
+};
+
+// vic.sex = "man";
