06ace95f8264aa205989824d8ac15519fb42e5de
yoshuu <appcs342@gmail.com>
2023-04-01 21:45:34 +0800

    book 06

---
+++ book06/main.ts
@@ -0,0 +1,12 @@
+// interface Person {
+//   name: String;
+//   age: number;
+//   // [apple: number]: any;
+// }
+
+// let tom: Person = {
+//   name: "vic",
+//   age: 10,
+// };
+
+let arr: Array<string> = ["a", "b", "c", "d"];
